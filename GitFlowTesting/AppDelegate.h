76dd5249fa682ac2c408ceffbf0631f273763560
Sarvjeet Singh <sarvjeet@techjini.com>
2014-09-25 13:30:37 +0530

    Added git locally

---
+++ GitFlowTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitFlowTesting
+//
+//  Created by TechJini on 9/25/14.
+//  Copyright (c) 2014 TechJini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitFlowTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitFlowTesting
+//
+//  Created by TechJini on 9/25/14.
+//  Copyright (c) 2014 TechJini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
